a8cd7478918a640a9b42b3d402984f442904f7a5
zMaffezzoli <brunomaffezzolirodrigues0@gmail.com>
2023-03-16 14:51:25 -0300

    Tudo funcionando!

--- serverPostPersistente/back-end/main.py
+++ serverPostPersistente/back-end/main.py
@@ -7,6 +7,7 @@ def incluir_pessoas():
 
     resposta = jsonify({"resultado":"Tudo certo! :)"})
     try:
+        db.create_all()
         nova_pessoa = Pessoa(**dados)
         db.session.add(nova_pessoa)
         db.session.commit()
@@ -17,4 +18,16 @@ def incluir_pessoas():
     resposta.headers.add("Access-Control-Allow-Origin", "*")
     return resposta 
 
+@app.route("/listar_pessoas")
+def listar_pessoas():
+    pessoas_json = []
+    pessoas = db.session.query(Pessoa).all()
+    for x in pessoas:
+        pessoas_json.append(x.json())
+
+    resposta = jsonify(pessoas_json)
+
+    resposta.headers.add("Access-Control-Allow-Origin", "*")
+    return resposta
+    
 app.run(debug=True)
